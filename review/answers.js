11b05a094d5c1a7d43a77dc4ca83e4ae47071ce8
Kristy Adams <kladams128@gmail.com>
2022-08-24 20:57:02 -0400

    yell at the ninja turtles answered

--- review/answers.js
+++ review/answers.js
@@ -1,63 +1,66 @@
 ////////////////////////////////////////////////
 ///Easy Going
 ////////////////////////////////////////////////
-for (let i = 1;i <= 20; i++){
-    console.log(i)
-}
+// for (let i = 1;i <= 20; i++){
+//     console.log(i)
+// }
 ////////////////////////////////////////////////
 ///Get Even
 ////////////////////////////////////////////////
-for (let i = 0; i <= 200; i += 2) {
-    console.log(i)
+// for (let i = 0; i <= 200; i += 2) {
+//     console.log(i)
     
-}
+// }
 
 ////////////////////////////////////////////////
 ///Fizz Buzz
 ////////////////////////////////////////////////
-for(let i = 0; i < 100; i++){
-    if(i % 15 === 0){
-        console. log("FizzBuzz")
-    }
-    else if(i % 3 === 0) {
-        console. log("Fizz")
-    }
-    else if(i % 5 === 0){
-        console.log("Buzz")
-    }
-    else{
-        console.log(i)
-    }
-}
+// for(let i = 0; i < 100; i++){
+//     if(i % 15 === 0){
+//         console. log("FizzBuzz")
+//     }
+//     else if(i % 3 === 0) {
+//         console. log("Fizz")
+//     }
+//     else if(i % 5 === 0){
+//         console.log("Buzz")
+//     }
+//     else{
+//         console.log(i)
+//     }
+// }
 
 ////////////////////////////////////////////////
 ///Wild Wild Life
 ////////////////////////////////////////////////
-const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
-const sharky = ["Sharky", "shark", 20, "Left Coast"]
-const plantee = ["Plantee", "plant", 5000, "Mordor"]
-const porgee = ["Porgee", "Porg", 186, "Ahch-to"]
-const dart = ["D'art", "Demogorgan Dog", 2, "Upside Down"]
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+// const sharky = ["Sharky", "shark", 20, "Left Coast"]
+// const plantee = ["Plantee", "plant", 5000, "Mordor"]
+// const porgee = ["Porgee", "Porg", 186, "Ahch-to"]
+// const dart = ["D'art", "Demogorgan Dog", 2, "Upside Down"]
 
-plantee[2] = 5001
-console.log(plantee)
+// plantee[2] = 5001
+// console.log(plantee)
 
-wolfy[3] = "Gotham City"
-console.log(wolfy)
+// wolfy[3] = "Gotham City"
+// console.log(wolfy)
 
-dart.push("Hawkins")
-console.log(dart)
+// dart.push("Hawkins")
+// console.log(dart)
 
-wolfy.shift()
-console.log(wolfy)
+// wolfy.shift()
+// console.log(wolfy)
 
-wolfy.unshift("Gameboy")
-console.log(wolfy)
+// wolfy.unshift("Gameboy")
+// console.log(wolfy)
 
 ////////////////////////////////////////////////
 ///Yell at the Ninja Turtles
 ////////////////////////////////////////////////
-
+const ninjaTurtles = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo']
+for (const turtle of ninjaTurtles) {
+    console.log(turtle.toUpperCase())
+}
 
 ////////////////////////////////////////////////
 ///Methods, Revisited
