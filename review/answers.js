907e82442f51cfb4679051d3c69a586c3e3c5bab
Kristy Adams <kladams128@gmail.com>
2022-08-28 16:30:15 -0400

    js review 2 complete

--- review/answers.js
+++ review/answers.js
@@ -1,75 +1,75 @@
 ////////////////////////////////////////////////
 ///Easy Going
 ////////////////////////////////////////////////
-// for (let i = 1;i <= 20; i++){
-//     console.log(i)
-// }
+for (let i = 1;i <= 20; i++){
+    console.log(i)
+}
 ////////////////////////////////////////////////
 ///Get Even
 ////////////////////////////////////////////////
-// for (let i = 0; i <= 200; i += 2) {
-//     console.log(i)
+for (let i = 0; i <= 200; i += 2) {
+    console.log(i)
     
-// }
+}
 
 ////////////////////////////////////////////////
 ///Fizz Buzz
 ////////////////////////////////////////////////
-// for(let i = 0; i < 100; i++){
-//     if(i % 15 === 0){
-//         console. log("FizzBuzz")
-//     }
-//     else if(i % 3 === 0) {
-//         console. log("Fizz")
-//     }
-//     else if(i % 5 === 0){
-//         console.log("Buzz")
-//     }
-//     else{
-//         console.log(i)
-//     }
-// }
+for(let i = 0; i < 100; i++){
+    if(i % 15 === 0){
+        console. log("FizzBuzz")
+    }
+    else if(i % 3 === 0) {
+        console. log("Fizz")
+    }
+    else if(i % 5 === 0){
+        console.log("Buzz")
+    }
+    else{
+        console.log(i)
+    }
+}
 
 ////////////////////////////////////////////////
 ///Wild Wild Life
 ////////////////////////////////////////////////
-// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
-// const sharky = ["Sharky", "shark", 20, "Left Coast"]
-// const plantee = ["Plantee", "plant", 5000, "Mordor"]
-// const porgee = ["Porgee", "Porg", 186, "Ahch-to"]
-// const dart = ["D'art", "Demogorgan Dog", 2, "Upside Down"]
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant", 5000, "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-to"]
+const dart = ["D'art", "Demogorgan Dog", 2, "Upside Down"]
 
-// plantee[2] = 5001
-// console.log(plantee)
+plantee[2] = 5001
+console.log(plantee)
 
-// wolfy[3] = "Gotham City"
-// console.log(wolfy)
+wolfy[3] = "Gotham City"
+console.log(wolfy)
 
-// dart.push("Hawkins")
-// console.log(dart)
+dart.push("Hawkins")
+console.log(dart)
 
-// wolfy.shift()
-// console.log(wolfy)
+wolfy.shift()
+console.log(wolfy)
 
-// wolfy.unshift("Gameboy")
-// console.log(wolfy)
+wolfy.unshift("Gameboy")
+console.log(wolfy)
 
 ////////////////////////////////////////////////
 ///Yell at the Ninja Turtles
 ////////////////////////////////////////////////
-// const ninjaTurtles = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo']
-// for (const turtle of ninjaTurtles) {
-//     console.log(turtle.toUpperCase())
-// }
+const ninjaTurtles = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo']
+for (const turtle of ninjaTurtles) {
+    console.log(turtle.toUpperCase())
+}
 
 ////////////////////////////////////////////////
 ///Methods, Revisited
 ////////////////////////////////////////////////
 const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious']
 
-//console.log(favMovies[8])
+console.log(favMovies[8])
 
-//console.log(favMovies.sort())
+console.log(favMovies.sort())
 //sorted the array in alphabetical order, it created a reference to originial array
 
 favMovies.push("Guardians of the Galaxy") // added to end of array
@@ -91,9 +91,9 @@ favMovies.splice(15,1, "Avatar")//removed django and add avatar
 const halfFavMovies = favMovies.splice(9,9)//removed half of array, permanently alters
 //console.log(favMovies)
 
-//console.log(halfFavMovies) //logs the half that was removed
+console.log(halfFavMovies) //logs the half that was removed
 
-//console.log(favMovies.indexOf("Fast and Furious"))// return zero
+console.log(favMovies.indexOf("Fast and Furious"))// return zero
 
 ////////////////////////////////////////////////
 ///Where is Waldo
@@ -108,14 +108,14 @@ whereIsWaldo.splice(1,1) //remove eggbert
 whereIsWaldo[1][2] = "No One"
 //console.log(whereIsWaldo)
 
-//console.log(whereIsWaldo[2][1][1])
+console.log(whereIsWaldo[2][1][1])
 
 ////////////////////////////////////////////////
 ///Excited Kitten
 ////////////////////////////////////////////////
-// for(let i = 0; i <= 20; i++){
-//     console.log("HSSSSSS")
-// }
+for(let i = 0; i <= 20; i++){
+    console.log("HSSSSSS")
+}
 
 
 let catMessages = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "why does the red dot always get away..."]
@@ -130,8 +130,6 @@ for(let i = 0; i <= 20; i++){
     }
     
 }
-
-
 
 ////////////////////////////////////////////////
 ///Find the Median
@@ -191,25 +189,25 @@ const thomsCloset = [
 ////////////////////////////////////////////////
 ///Alien Attire
 ////////////////////////////////////////////////
-//console.log(kristynsCloset)
+console.log(kristynsCloset)
 
 const kristynsShoe = kristynsCloset.shift();
-//console.log(kristynsShoe)
+console.log(kristynsShoe)
 
 thomsCloset.push(kristynsShoe)
-//console.log(thomsCloset)
+console.log(thomsCloset)
 
 ////////////////////////////////////////////////
 ///Dress Us Up
 ////////////////////////////////////////////////
 
-// console.log("Kristyn is wearing a " + kristynsCloset[2]+ ", " + kristynsCloset[3]+ ", and a " + kristynsCloset[4])
-// console.log("Kristyn is wearing " + kristynsCloset[0]+ ", " + kristynsCloset[4]+ ", and a " + kristynsCloset[5])
-// console.log("Kristyn is wearing " + kristynsCloset[1]+ ", " + kristynsCloset[2]+ ", and a " + kristynsCloset[3])
+console.log("Kristyn is wearing a " + kristynsCloset[2]+ ", " + kristynsCloset[3]+ ", and a " + kristynsCloset[4])
+console.log("Kristyn is wearing " + kristynsCloset[0]+ ", " + kristynsCloset[4]+ ", and a " + kristynsCloset[5])
+console.log("Kristyn is wearing " + kristynsCloset[1]+ ", " + kristynsCloset[2]+ ", and a " + kristynsCloset[3])
 
-// console.log("Thom is wearing a " + thomsCloset[0][1]+ ", " + thomsCloset[1][2]+ ", and a " + thomsCloset[2][1])
-// console.log("Thom is wearing a " + thomsCloset[0][3]+ ", " + thomsCloset[1][1]+ ", and " + thomsCloset[2][2])
-// console.log("Thom is wearing a " + thomsCloset[0][2]+ ", " + thomsCloset[1][0]+ ", and " + thomsCloset[2][0])
+console.log("Thom is wearing a " + thomsCloset[0][1]+ ", " + thomsCloset[1][2]+ ", and a " + thomsCloset[2][1])
+console.log("Thom is wearing a " + thomsCloset[0][3]+ ", " + thomsCloset[1][1]+ ", and " + thomsCloset[2][2])
+console.log("Thom is wearing a " + thomsCloset[0][2]+ ", " + thomsCloset[1][0]+ ", and " + thomsCloset[2][0])
 
 ////////////////////////////////////////////////
 ///Dirty Laundry
@@ -219,9 +217,9 @@ thomsCloset.push(kristynsShoe)
     }
 
 /////////////////////////////////////////////////
-///Inventory <--- ASK ABOUT THIS --->
+///Inventory
 ////////////////////////////////////////////////
 
-// console.log(thomsCloset[0])
-// console.log(thomsCloset[1])
-// console.log(thomsCloset[2])
+console.log(thomsCloset[0])
+console.log(thomsCloset[1])
+console.log(thomsCloset[2])
